cd14c1b25ac3488938868fda4c649a5f0a0e26b9
Bazinga <ki5syn333@gmail.com>
2025-12-10 13:18:08 +0300

    second commit mole-game

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -2,18 +2,32 @@ let hit = Number(document.getElementById("dead").textContent);
 let miss = Number(document.getElementById("lost").textContent);
 
 
-    for(let i = 1; i <= 9; i++) {
-        let hole  =  document.getElementById(`hole${i}`)    
-        hole.onclick = () => {
-        if ( hole.className.includes("hole_has-mole")) {
-            hit++;
-            document.getElementById("dead").textContent = hit;
-        } else {
-            miss++;
-            document.getElementById("lost").textContent = miss;
-        } 
+for (let i = 1; i <= 9; i++) {
+	let hole = document.getElementById(`hole${i}`)
+	hole.onclick = () => {
+		if (hole.className.includes("hole_has-mole")) {
+			hit++;
+			document.getElementById("dead").textContent = hit;
+		} else {
+			miss++;
+			document.getElementById("lost").textContent = miss;
+		}
+
+		if (hit === 10) {
+			alert("Вы победили")
+			hit = 0;
+			miss = 0;
+		};
+
+		if (miss === 5) {
+			alert("Вы проиграли");
+			hit = 0;
+			miss = 0;
+		}
+
+		document.getElementById("dead").textContent = hit;
+		document.getElementById("lost").textContent = miss;
+	}
+
+} 
         
-        }
-    }
-
-
