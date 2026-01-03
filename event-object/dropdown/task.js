a2ef75261b927217897ce0c94d932884cadf5b10
Bazinga <ki5syn333@gmail.com>
2026-01-03 15:03:03 +0300

    first commit dropdown

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,28 @@
+const onList = (listElement) => {
+	listElement.classList.add("dropdown__list_active")
+}
+
+const closeList = (listElement) => {
+	listElement.classList.remove("dropdown__list_active")
+}
+
+document.addEventListener("click", (event) => {
+	let findGroup = event.target.closest(".dropdown")
+	let findValua = findGroup.querySelector(".dropdown__value")
+	let findItem = event.target.closest(".dropdown__item")
+
+	if (event.target.classList.contains("dropdown__link")) {
+		event.preventDefault()
+	}
+
+	if (findItem) {
+		closeList(findGroup.querySelector(".dropdown__list"))
+		findValua.textContent = findItem.querySelector(".dropdown__link").textContent;
+
+	}
+
+	if (event.target.classList.contains("dropdown__value")) {
+		onList(document.querySelector(".dropdown__list"))
+	}
+
+})
