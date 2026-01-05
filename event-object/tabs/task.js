797e3e8d501ad276ff6b867a0d43bce32cf63062
Bazinga <ki5syn333@gmail.com>
2026-01-05 18:58:53 +0300

    first commit tabs

---
+++ event-object/tabs/task.js
@@ -0,0 +1,17 @@
+let navigation = document.querySelector(".tab__navigation")
+let contents = document.querySelectorAll(".tab__content")
+
+document.addEventListener("click", (event) => {
+	navigation.querySelectorAll(".tab").forEach((e, index) => {
+		if (e === event.target) {
+			e.classList.add("tab_active")
+			contents[index].classList.add("tab__content_active")
+		} else {
+			e.classList.remove("tab_active")
+			contents[index].classList.remove("tab__content_active")
+		}
+
+	})
+
+
+})
