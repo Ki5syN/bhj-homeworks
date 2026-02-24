5f90fb07c1bf978a3f4e3d1eee77687b173fef41
Bazinga <ki5syn333@gmail.com>
2026-02-24 13:15:43 +0300

    second commit auth

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,3 +1,16 @@
+document.addEventListener("DOMContentLoaded", ()=> {
+    const numUser  = localStorage.getItem("user")
+    if(numUser) {
+        change(numUser)
+    }
+})
+
+function change (numUser) {
+    document.getElementById("signin").classList.remove("signin_active")
+    document.getElementById("welcome").classList.add("welcome_active")
+    document.getElementById("user_id").textContent = numUser 
+}
+
 const form = document.getElementById("signin__form");
 
 form.addEventListener("submit", event => {
@@ -9,15 +22,15 @@ form.addEventListener("submit", event => {
         body: formData
     })
     .then (response => response.json())
-    .then(data =>{
+    .then(data => {
         console.log(data)
         if (data.success) {
-            document.getElementById("signin").classList.remove("signin_active")
-            document.getElementById("welcome").classList.add("welcome_active")
-            document.getElementById("user_id").textContent = data.user_id
+            const numUser = data.user_id
+            localStorage.setItem("user", numUser)
+            form.reset()
+            change (numUser)            
         } else {
             alert("Неверные данные логин/пароль")
         }
     })
-
 })
